6099b48383548c55980569bf2fb31570752189e4
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-07-22 20:47:44 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 describe('index.js', function () {
   describe('displayTime()', function () {
     it('can successfully access moment() from the momentjs node module', function () {
-      expect(moment()).to.exist
+      expect(moment()).to.exist;
     });
   });
 });
